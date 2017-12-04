67c69fbc55a78e6724403e71b1f877e4093a411e
phoet <phoetmail@googlemail.com>
2017-12-04 15:24:06 +0100

    expose some convenience methods

--- app/models/time_zone.rb
+++ app/models/time_zone.rb
@@ -5,12 +5,19 @@ class TimeZone < ApplicationRecord
 
   scope :by_name, -> (name) { where('lower(name) LIKE lower(?)', "%#{name}%") }
 
+  def time_with_zone
+    ActiveSupport::TimeZone.new(key)
+  end
+
   def current_time
-    tz = ActiveSupport::TimeZone.new(key)
-    tz.now
+    time_with_zone.now
+  end
+
+  def utc_offset
+    time_with_zone.utc_offset
   end
 
   def as_json(options = {})
-    super(options.merge(methods: :current_time))
+    super(options.merge(methods: [:current_time, :utc_offset]))
   end
 end
